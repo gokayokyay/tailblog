f00ee4dd06fe5cbd93e2e84e7685ba554f44a952
gokayokyay <gokayokyay@hotmail.com>
2020-06-28 21:39:30 +0000

    Initial

---
+++ src/components/Home/Feed.svelte
@@ -0,0 +1,8 @@
+<script>
+  import PostCard from '../Common/PostCard.svelte';
+</script>
+<div class="flex flex-wrap p-4 w-full">
+  <PostCard />
+  <PostCard />
+  <PostCard on:click={e => alert(e)}/>
+</div>
