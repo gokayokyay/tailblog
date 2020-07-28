a2ff88f465bdec95382a7f3540c7e831d16ba4ca
gokayokyay <gokayokyay@hotmail.com>
2020-07-28 23:39:34 +0100

    post page changes

--- src/routes/admin/preview.svelte
+++ src/routes/admin/preview.svelte
@@ -12,18 +12,17 @@
   .author > img {
     max-width: 75%;
   }
-  
 </style>
 
 <div class="post-page-root">
-  <div class="author absolute w-32 h-28 top-0 mt-8 left-0 right-0 m-auto">
+  <div class="author absolute w-40 lg:w-32 h-28 top-0 mt-8 left-0 right-0 m-auto">
     <img class="m-auto border-4 border-gray-100 rounded-full cover" src="https://i.pravatar.cc/300" alt="avatar" />
     <p class="font-bold text-center">John Smith</p>
   </div>
   <div class="post-page-main">
-    <div class="post-page-head">
+    <div class="post-page-head mb-8">
       <div class="post-page-metadata">
-        <p class="text-xs text-right text-gray-500 italic">Created at 23 July 2020</p>
+        <p class="mb-2 text-xs text-right text-gray-500 italic">Created at 23 July 2020</p>
       </div>
       <h1 class="text-center">
         <!-- {post.title} -->
@@ -37,5 +36,8 @@
     </div>
     This is content
     <!-- {@html post.content} -->
+  </div>
+  <div class="post-page-recommends-container">
+    
   </div>
 </div>
